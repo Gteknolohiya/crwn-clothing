6f2c46d506bcc58e53bc64de4d284bfa2d1589a6
Angelo Baricante <gteknolohiya@gmail.com>
2022-09-09 15:02:25 +0800

    added cart item component

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,13 +1,27 @@
 import { createContext, useState } from "react";
 
+const addCartItem = (cartItems, productToAdd) => {
+    // find if cartItems contains producToAdd 
+
+    // If found increment quantity
+
+    // Return array with modified cartItems / New cart items
+}
+
 export const CartContext = createContext({
     isCartOpen: false,
-    setIsCartOpen: () => {}
+    setIsCartOpen: () => {},
+    cartItem: [],
+    addItemToCart: () => {}
 });
 
 export const CartProvider = ({children}) => {
     const [isCartOpen, setIsCartOpen] = useState(false);
     const value = {isCartOpen, setIsCartOpen};
+
+    const addItemToCart =  (productToAdd) => {
+         
+    }
 
     return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 }
