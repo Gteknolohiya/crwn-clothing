c1ca7903a8101f9bccd943000651d2a6b181a13a
Angelo Baricante <gteknolohiya@gmail.com>
2022-09-21 05:30:43 +0800

    users can now remove items from the checkout page

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -18,11 +18,33 @@ const addCartItem = (cartItems, productToAdd) => {
     return [...cartItems, { ...productToAdd, quantity: 1 }];
 }
 
+// Remove item to cart
+const removeCartItem = (cartItems, cartItemToRemove) => {
+    // find the cart item to remove
+    const existingCartItem = cartItems.find(
+        (cartItem) => cartItem.id === cartItemToRemove.id
+    ); 
+
+    // check if quantity is equal to 1, if it is remove that item to cart
+    if (existingCartItem.quantity === 1) {
+        return cartItems.filter(cartItem => cartItem.id != cartItemToRemove.id);
+    }  
+
+    // return back cartItems with matching cart item with reduced quantity
+    return cartItems.map((cartItem) => 
+    cartItem.id === cartItemToRemove.id ?
+    { ...cartItem, quantity: cartItem.quantity - 1 } 
+    : cartItem
+);
+}
+
 export const CartContext = createContext({
     isCartOpen: false,
     setIsCartOpen: () => {},
     cartItems: [],
     addItemToCart: () => {},
+    removeItemToCart: () => {},
+    removeCartItem: () => {},
     cartCount: 0
 });
 
@@ -37,11 +59,24 @@ export const CartProvider = ({children}) => {
         setCartCount(newCartCount);
     }, [cartItems])
 
+    // Add item to cart
     const addItemToCart =  (productToAdd) => {
         setCartItems(addCartItem(cartItems, productToAdd));
    }
 
-    const value = {isCartOpen, setIsCartOpen, addItemToCart, cartItems, cartCount};
+   // Remove cart item
+   const removeItemToCart = (cartItemToRemove) => {
+        setCartItems(removeCartItem(cartItems, cartItemToRemove));
+   }
+
+    const value = {
+        isCartOpen, 
+        setIsCartOpen, 
+        addItemToCart, 
+        removeItemToCart,
+        cartItems, 
+        cartCount
+    };
 
     return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 }
